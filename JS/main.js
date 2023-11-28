a33ddbd76679d5782337c14d8c9cccf78a6bfef7
Officialhomie <vicigwilo@gmail.com>
2023-11-29 00:34:11 +0100

    starting JavaScript

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,9 @@
-!
+const faqs = document.querySelectorAll(".faq");
+
+faqs.forEach(faqq =>{
+    faqq.addEventListener("click", ()=>{
+        faqq.classList.toggle("active")
+    })
+})
+
+console.log(faqs)
