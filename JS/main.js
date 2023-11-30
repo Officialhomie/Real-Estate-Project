aaaf39766f014ec862566b13b77a4619f5a29c46
Officialhomie <vicigwilo@gmail.com>
2023-11-30 17:00:04 +0100

    enabling js and media reponsiveness

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,8 @@
 const faqs = document.querySelectorAll(".faq");
+const navIcons = document.querySelector('nav i');
+const nav = document.querySelector("nav")
+
+
 
 faqs.forEach(faqq =>{
     faqq.addEventListener("click", ()=>{
@@ -6,4 +10,70 @@ faqs.forEach(faqq =>{
     })
 })
 
-console.log(faqs)
+
+function toggleDisplay() {
+    console.log("dfgh");
+    nav.classList.toggle("show-nav")
+}
+navIcons.addEventListener("click", toggleDisplay)
+
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        loop:true,
+        responsiveClass:true,
+        autoplay:true,
+        navText: ["<i class='bi bi-chevron-left'></i>", "<i class='bi bi-chevron-right'></i>"],
+        responsive:{
+            0:{
+                items:1,
+                nav:true
+            },
+            700:{
+                items:2,
+                nav:false,
+                margin:50
+            },
+            1000:{
+                items:3,
+                nav:true,
+                // loop:false
+            },
+            1200:{
+                items:4,
+                nav:true,
+                // loop:false
+            },
+            1500:{
+                items:5,
+                nav:true,
+                // loop:false
+            }
+        }
+        });
+  });
+
+$(document).ready(function(){
+    $(".owl-carousel-1").owlCarousel({
+        loop:true,
+        responsiveClass:true,
+        autoplay:true,
+        margin:50,
+        responsive:{
+            0:{
+                items:1,
+                // nav:true
+            },
+            700:{
+                items:2,
+                nav:false,
+                margin:50
+            },
+            1000:{
+                items:3,
+                // nav:true,
+                loop:false
+            }
+        }
+        });
+  });
+
